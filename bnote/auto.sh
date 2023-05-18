33f4b3a16bc6d7fba8ac366689b18ab938d4ea16
zhengkai <zhengkai.chen@ersoft.cn>
2023-05-18 23:21:21 +0800

    flask

--- bnote/auto.sh
+++ bnote/auto.sh
@@ -4,17 +4,20 @@ source ~/.zshrc
 # 读取Title,文件名
 temp=$1
 title=${*/$1/}
-filename=`echo $title |sed -e 's/[^A-Za-z0-9*]/-/g'|sed -e 's/[-][-]*/-/g'| sed -e 's/-$//g'|awk '{print tolower($1)}'`
+title=`echo $title|sed -e 's/^[[:space:]]*//'`
+filename=`echo $title |sed -e 's/[^A-Za-z0-9*]/-/g'|sed -e 's/[-][-]*/-/g'| sed -e 's/-$//g' |awk '{print tolower($1)}'`
+filepath=source/_posts/$filename.md
+
 # 新建文章md
 if [ -e $filepath ];then
-    filename=$temp-`date +%Y%m%d%H%M%S`
+    filename=$filename-`date +%Y%m%d%H%M%S`
+    filepath=source/_posts/$filename.md
 fi
 
-filepath=source/_posts/$filename.md
 echo title=$title
 echo filepath=$filepath
- 
-docker run -i --rm --volume $PWD:/data:rw --volume $HOME:/root:rw --workdir /data hkccr.ccs.tencentyun.com/zhengkai/hexo:20-alpine hexo new $temp -s $filename $title
+
+docker run -i --rm --volume $PWD:/data:rw --volume $HOME:/root:rw --workdir /data hkccr.ccs.tencentyun.com/zhengkai/hexo:20-alpine hexo new $temp.md -s $filename $title
 
 # 用chatgpt生成内容并追加到文章中
 data="{\"message\":\"${title}, 500字以上\"}"
